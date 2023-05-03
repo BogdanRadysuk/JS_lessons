c9d07e699bc4a385d3c37e8f940abd84799cd7a4
BogdanRadysuk <radysuk2005@gmail.com>
2023-05-03 19:11:13 +0300

    hw1

---
+++ lesson1/main.js
@@ -0,0 +1,41 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log
+// let qwe = 'hello'
+// let asd = 'owu'
+// let art = 'com'
+// let zxc = 'ua'
+// let n1 = 1
+// let n2 = 10
+// let n3 = -999
+// let n4 = 123
+// let n5 = 3.14
+// let n6 = 2.7
+// let n7 = 16
+// let type = true
+// let value = false
+//
+// console.log(qwe, asd, art, zxc, n1, n2, n3, n4, n5, n6, n7, type, value)
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+// let firstName = 'Bogdan'
+// let middleName = 'Radysuk'
+// let lastName = 'Andriyovych'
+//
+// let initials = `${firstName}, ${middleName}, ${lastName}`
+// console.log(initials)
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+// let a = 100;
+// let b = '100';
+// let c = true;
+//
+// console.log(typeof a)
+// console.log(typeof b)
+// console.log(typeof c)
+
+// Додаткове для тих хто цікавився prompt`oм
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+// let surname = prompt('whats your surname?')
+// let name = prompt('whats your name?')
+// let lastname = prompt('whats your lastname?')
+// console.log(name, surname, lastname)
