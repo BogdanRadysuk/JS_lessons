831939d084a7179754ec398cc71ba20d817cb714
BogdanRadysuk <radysuk2005@gmail.com>
2023-05-15 21:15:03 +0300

    hw

--- lesson4/main.js
+++ lesson4/main.js
@@ -65,18 +65,69 @@
 
 // - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ.
 // Для кожного об'єкту окремий блок.
-function asdqwe() {
-    const user = {id}
-}
-
-
-
+// const users = [
+//     {name: 'vasya', age: 31, id: 1},
+//     {name: 'petya', age: 30, id: 2},
+//     {name: 'kolya', age: 29, id: 3},
+//     {name: 'olya', age: 28, id: 4},
+//     {name: 'max', age: 30, id: 1},
+//     {name: 'anya', age: 31, id: 2},
+//     {name: 'oleg', age: 28, id: 3},
+//     {name: 'andrey', age: 29, id: 4},
+//     {name: 'masha', age: 30, id: 5},
+//     {name: 'olya', age: 31, id: 6},
+//     {name: 'max', age: 31, id: 7}
+// ];
+// function asdqwe(user) {
+//     for (const userElement of user) {
+//         document.write(`<div>id: ${userElement.id} ${userElement.name} ${userElement.age} </div>`)
+//     }
+// }
+// asdqwe(users)
 
 // - створити функцію яка повертає найменьше число з масиву
+// const arrayOfNumbers = [1, 2]
+// function numb(smallest) {
+//     const result = smallest[0];
+//     for (let i = 1; i < smallest.length; i++) {
+//         if (result > smallest[i]){
+//             result = smallest[i]
+//         }
+//     }
+//     return result
+// }
+// console.log(numb(arrayOfNumbers));
+
 // - створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його.
 // Приклад sum([1,2,10]) //->13
+// const sum = [1,5,8,20]
+// function sumOfNumbers(numb) {
+//     let result = 0
+//     for (const number of numb) {
+//         result += number
+//     }
+//     return result
+// }
+// console.log(sumOfNumbers(sum));
 
 // - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
 // Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
+// function swap(arr, index1, index2) {
+//     const value1 = arr[index1]
+//     const value2 = arr[index2]
+//     arr[index1] = value2
+//     arr[index2] = value1
+//     return arr
+// }
+// console.log(swap([193402, 29024, 39351], 0, 1));
+
 // - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 // Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250
+// function exchange(sumUAH, currencyValues, exchangeCurrency) {
+//     for (const item of currencyValues) {
+//         if (item.currency === exchangeCurrency){
+//             return sumUAH / item.value
+//         }
+//     }
+// }
+// console.log(exchange(10000, [{currency: 'USD', value: 40}, {currency: 'EUR', value: 42}], 'EUR'));
