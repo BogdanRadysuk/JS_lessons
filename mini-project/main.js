9d6dd26603f1d00a2554bf7005812f74abc030bb
BogdanRadysuk <radysuk2005@gmail.com>
2023-06-16 15:55:25 +0300

    final

--- mini-project/main.js
+++ mini-project/main.js
@@ -1,11 +1,15 @@
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(res => res.json())
     .then(users => {
+        let divMain = document.createElement('div')
+        divMain.classList.add('divMain')
         for (const user of users) {
+            // let divA =document.createElement()
             let div = document.createElement('a')
-            div.classList.add('mainDiv')
+            div.classList.add('div')
             div.innerText = `${user.id} ${user.name}`
             div.href = 'user-details.html?id=' + user.id
-            document.body.append(div)
+            divMain.appendChild(div)
         }
+            document.body.append(divMain)
     })
--- mini-project/user-details.js
+++ mini-project/user-details.js
@@ -10,56 +10,46 @@ let h3Info = document.createElement('h3')
 let divButton = document.createElement('div')
 divButton.classList.add('divButton')
 
-
-//     user-details.html - блок з інфою про user зверху сторінки. Кнопка нижчє, на 90% ширини сторінки, по центру.
-//     блоки з короткою іфною про post - в ряд по 5 .
-//search user with him ID
 fetch('https://jsonplaceholder.typicode.com/users/' + urlID)
     .then(res => res.json())
     .then(user => {
-        // div.innerText = JSON.stringify(user)
         h2Id.innerText = `id:${user.id}  Name:${user.name}`
         h2Info.innerText = `username: "${user.username}" email: "${user.email}" phone: ${user.phone}`
         h3Info.innerText = `website: ${user.website}`
-        // divAddress.innerText = `address:`
-        divAddress.innerHTML= `<h1>Address:</h1>
+        divAddress.innerHTML = `<h1>Address:</h1>
 <h2>City:${user.address.city} Street:${user.address.street} </h2> 
 <h3>
 Suite:${user.address.suite} zip:${user.address.zipcode} 
 <h4>geo:  lat:${user.address.geo.lat} ing:${user.address.geo.lng}</h4>
 </h3>`
         divCompany.innerHTML = `<h1>Company:</h1> <h2>name: ${user.company.name}</h2> <h3>catchPhrase: ${user.company.catchPhrase}</h3> <h3>bs: ${user.company.bs}</h3>`
-
-
-
-
-
-        console.log(user)
-        div.append(h2Id, h2Info, divAddress, divCompany,h3Info)
+        div.append(h2Id, h2Info, divAddress, divCompany, h3Info)
     })
 
 fetch('https://jsonplaceholder.typicode.com/users/' + urlID + '/posts')
     .then(res => res.json())
     .then(post => {
-        // for (const postElement of post) {
-        //
-        // console.log(postElement.id)
-        // }
         let button = document.createElement('button')
         button.innerText = 'post of current user'
         button.classList.add('buttonLi')
 
         let ulPosts = document.createElement('ul')
+        ulPosts.classList.add('ulPosts')
+        let divUl = document.createElement('div')
+        divUl.appendChild(ulPosts)
+        divUl.classList.add('divUl')
+
         button.onclick = function () {
             for (const divPost of post) {
                 let li = document.createElement('li');
+                li.classList.add('liPosts')
                 let aPosts = document.createElement('a')
-                aPosts.innerHTML += divPost.title
+                aPosts.innerHTML += 'Post:' + ' ' + divPost.title
                 aPosts.href = 'post-details.html?id=' + divPost.id
                 li.appendChild(aPosts)
                 ulPosts.appendChild(li)
             }
         }
         divButton.appendChild(button)
-        document.body.append(div, divButton,ulPosts)
+        document.body.append(div, divButton, divUl)
     })
